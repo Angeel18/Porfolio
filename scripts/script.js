79f4668b71c22cdc9e485be626c2cb7245126fd0
Angel <angeel1824@gmail.com>
2024-11-16 17:39:30 +0100

    language and about me done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
-function test(){
-    document.getElementById("header").style.backgroundColor="black"
-}
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".header-content a");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                navLinks.forEach((link) => link.classList.remove("active"));
+
+                const activeLink = document.querySelector(
+                    `.header-content a[href="#${entry.target.id}"]`
+                );
+                if (activeLink) activeLink.classList.add("active");
+            }
+        });
+    },
+    { threshold: 0.6 }
+);
+
+sections.forEach((section) => {
+    observer.observe(section);
+});
